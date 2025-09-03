cdd683e60ffe1ca385e73d359c402a4d51489d33
unknown <cnh665@gmail.com>
2025-09-03 17:16:44 +0900

    250903 tensorflow와 pytorch 실습 및 강의 자료 그리고 쳇gpt 통해 회차별로 로또 당첨번호 가져오고 다음 회 로또 번호 생성해주는 프로그램 및 파일 생성 폴더 개발

---
+++ python_Spyder_example/250903_순환신경망_pytorch_최종_조창제강사.py
@@ -0,0 +1,319 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  3 12:46:10 2025
+
+@author: human
+"""
+
+import glob
+import numpy as np 
+import pandas as pd 
+import torch
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import MinMaxScaler
+import pytorch_lightning as pl
+
+"""
+1. 전체 데이터 불러오기 
+2. 시간 결측 없게 join 활용해서 만들기
+3. df.interpolate(method="linear") 활용하여 보간하기
+4. 2023, 2024년도를 기준으로 자료 MinMax Scaling 하기
+5. 학습형식에 맞게 자료를 
+    t-23, t-22... t-1, 현재, t+1, t+2, t+3 형태로 만들기
+6. 2023, 2024년도 Train데이터와 validation 데이터로 만들기
+7. 2025년도 Test 데이터로 사용하기
+8. 데이터셋 / 데이터로더 만들기
+9. 모델 구축하기
+10. 모델 학습하기
+11. 모델 예측하기
+12. 예측값 원래의 기온 범위로 복원하기(re scaling)
+13. MAE와 같은 지표로 성능평가하기
+"""
+
+file_dir = "E:/cjcho_work/250903/data"
+# 전체 데이터 읽어와서 하나로 합치기 14분 시작
+# 3개 파일 읽어와서 df 라는 이름으로 합치기
+# Hint: pd.concat([], axis=0)
+
+files = glob.glob(f"{file_dir}/*.csv")
+df1 = pd.read_csv(files[0], encoding="cp949")
+df2 = pd.read_csv(files[1], encoding="cp949")
+df3 = pd.read_csv(files[2], encoding="cp949")
+
+df = pd.concat([df1, df2, df3], axis=0, ignore_index=True)
+
+df.isna().sum()
+df["일시"] = pd.to_datetime(df["일시"])
+(df["일시"].diff()).value_counts()
+# 2시간 없는 경우 1건 
+# 1시간 없는 경우 1건
+
+min_dt = df["일시"].min()
+max_dt = df["일시"].max()
+alltimes = pd.date_range(min_dt, max_dt, freq="h")
+time_df = pd.DataFrame({"일시":alltimes})
+
+## pd.merge 활용해서 df와 time_df 결합하기 24분 
+df.shape
+pd.merge(df, time_df, on="일시", how="right").shape
+pd.merge(df, time_df, on="일시", how="left").shape
+pd.merge(df, time_df, on="일시", how="outer").shape
+pd.merge(df, time_df, on="일시", how="inner").shape
+
+tot_df = pd.merge(df, time_df, on="일시", how="right")
+tot_df = tot_df.iloc[:,2:]
+tot_df.columns = ["times", "temp"]
+# 2시간 연속 결측 1건
+# 1시간 결측 1건
+(df["일시"].diff()).value_counts() 
+tot_df[tot_df["temp"].isna()]
+
+# 35분 진행
+np.where(tot_df["temp"].isna()) # [8152, 8153]
+checked_index = list(range(8149, 8158))
+tot_df.loc[checked_index] # 보간되기전 값 확인
+tot_df = tot_df.interpolate(method="linear")
+tot_df.loc[checked_index] # 보간된 결과값 확인
+
+scaler = MinMaxScaler(feature_range=(-1, 1))
+scaler
+"""
+2023, 2024 자료를 바탕으로 -1~1 범위로 
+    변경되게 min값 max 값을 설정
+"""
+tr_index = tot_df["times"].dt.year.isin([2023,2024])
+# tr_index = tot_df["times"].dt.year!=2025
+scaler.fit(
+    tot_df.loc[tr_index,["temp"]])
+# 2023/2024년도 데이터 min max 스케일링 반영
+tot_df.loc[tr_index,["temp"]] = scaler.transform(
+    tot_df.loc[tr_index,["temp"]])
+
+# 2025년도 데이터 min max 스케일링 반영
+tot_df.loc[~tr_index,["temp"]] = scaler.transform(
+    tot_df.loc[~tr_index,["temp"]])
+
+# 3시 3분 진행
+
+"""
+5. 학습형식에 맞게 자료를 
+    t-23, t-22... t-1, 현재, t+1, t+2, t+3 형태로 만들기
+3시 13분까지 진행
+df.shift활용
+pd.concat 활용
+"""
+tot_df
+tot_df.shift(1)
+tot_df.shift(2)
+tot_df.shift(3)
+
+pd.concat([
+    tot_df,
+    tot_df["temp"].shift(1),
+    tot_df["temp"].shift(2),
+    tot_df["temp"].shift(3),
+    ],axis=1)
+
+# shift 23 ~ shift 1
+dfs = []
+list(range(23, 0, -1))
+for i in list(range(23, 0, -1)):
+    _df = tot_df["temp"].shift(i)
+    dfs.append(_df)
+pd.concat(dfs,axis = 1)
+
+# -23시간전, -22시간전, ..., 현재시간, 1시간후, 2시간후, 3시간후
+dfs = []
+list(range(23, -4, -1))
+for i in list(range(23, -4, -1)):
+    _df = tot_df["temp"].shift(i)
+    dfs.append(_df)
+train_form_df = pd.concat(dfs,axis = 1)
+train_form_df.columns = [f"lag_{i}" for i in list(range(23, -4, -1))]
+
+train_form_df = pd.concat([tot_df[["times"]], train_form_df], axis=1)
+train_form_df = train_form_df.dropna().reset_index(drop=True)
+### 정리하고 26분 진행6. 2023, 2024년도 Train데이터와 validation 데이터로 만들기
+
+"""
+# 33분까지
+6. 2023, 2024년도 Train데이터와 validation 데이터로 만들기
+7. 2025년도 Test 데이터로 사용하기
+"""
+tr_index = train_form_df["times"].dt.year.isin([2023, 2024])
+tr_df = train_form_df[tr_index] # 2023, 2024년도 데이터 추출
+te_df = train_form_df[~tr_index] # 2025년도 데이터 추출
+te_df = te_df.reset_index(drop = True)
+
+tr_df, val_df = train_test_split(
+    tr_df, test_size=0.1, shuffle=True, random_state=42)
+
+
+## 남은 시간동안 시간단위 데이터를 일단위 데이터로 바꿔서 시각화 해보기
+
+import matplotlib.pyplot as plt 
+#%matplotlib auto
+df_2023 = train_form_df[train_form_df["times"].dt.year==2023].copy()
+df_2024 = train_form_df[train_form_df["times"].dt.year==2024].copy()
+df_2025 = train_form_df[train_form_df["times"].dt.year==2025].copy()
+plt.plot(range(df_2023.shape[0]), df_2023["lag_0"], alpha=0.3)
+plt.plot(range(df_2024.shape[0]), df_2024["lag_0"], alpha=0.3)
+plt.plot(range(df_2025.shape[0]), df_2025["lag_0"], alpha=0.3)
+
+plot_df = train_form_df.copy()
+plot_df["date"] = plot_df["times"].dt.date
+plot_df = plot_df.drop(
+    "times",axis=1).groupby("date").mean().reset_index()
+
+plot_df["date"] = pd.to_datetime(plot_df["date"])
+df_2023 = plot_df[plot_df["date"].dt.year == 2023]
+df_2024 = plot_df[plot_df["date"].dt.year == 2024]
+df_2025 = plot_df[plot_df["date"].dt.year == 2025]
+
+plt.plot(range(df_2023.shape[0]), df_2023["lag_0"], alpha=0.3)
+plt.plot(range(df_2024.shape[0]), df_2024["lag_0"], alpha=0.3)
+plt.plot(range(df_2025.shape[0]), df_2025["lag_0"], alpha=0.3)
+
+"""
+9. 모델 구축하기
+10. 모델 학습하기
+11. 모델 예측하기
+12. 예측값 원래의 기온 범위로 복원하기(re scaling)
+13. MAE와 같은 지표로 성능평가하기
+"""
+
+"""
+8. 데이터셋 / 데이터로더 만들기
+     > index 입력하면, x값이랑 y 값이 출력되게 하기
+     > dataset -> 1개 데이터 기준
+"""
+
+
+index = 0 
+batch_size = 4
+tr_df.iloc[:,1:25]
+tr_df.iloc[:,25:]
+x_index = list(range(1, 25))
+y_index = list(range(25,28))
+tr_df.iloc[:,x_index]
+tr_df.iloc[:,y_index]
+"""
+0~ 4 index에 해당하는 데이터 추출하고
+추출된 데이터 x에 해당하는 행, y에 해당하는 행
+
+위에서 진행한 것 활용해서 getitem_fun 구현하기 4시 8분 시작.
+getitem_fun은 data랑 index, x_index, y_index 입력하면
+index 에 해당하는 x, y 값 나오는 함수
+"""
+
+def getitem_fun(df, index, x_index, y_index):
+    x = df.iloc[index ,x_index].values
+    y = df.iloc[index ,y_index].values
+    return x, y
+
+x,y = getitem_fun(tr_df, 0, x_index, y_index)
+x.shape, y.shape
+
+tr_df.shape[0]
+len(tr_df)
+class RNNDataset(torch.utils.data.Dataset):
+    def __init__(self, df, x_index, y_index, getitem_fun):
+        self.df = df 
+        self.x_index = x_index
+        self.y_index = y_index
+        self.getitem_fun = getitem_fun
+    def __len__(self):
+        # 전체 데이터의 길이 
+        return self.df.shape[0]
+    def __getitem__(self, index):
+        x, y = self.getitem_fun(
+            self.df, index, self.x_index, self.y_index)
+        x = x.astype(np.float32)
+        y = y.astype(np.float32)
+        x = torch.tensor(x, dtype = torch.float32)
+        y = torch.tensor(y, dtype = torch.float32)
+        return x, y
+
+tr_dataset = RNNDataset(tr_df, x_index, y_index, getitem_fun)
+val_dataset = RNNDataset(val_df, x_index, y_index, getitem_fun)
+x,y= next(iter(tr_dataset))
+x.shape, y.shape
+tr_dataloader = torch.utils.data.DataLoader(
+    tr_dataset, batch_size=32, shuffle=True)
+val_dataloader = torch.utils.data.DataLoader(
+    val_dataset, batch_size=32, shuffle=True)
+
+x,y= next(iter(tr_dataloader))
+x.shape, y.shape
+
+## 24분 시작
+
+from torch import nn 
+import pytorch_lightning as pl
+# input, output, 체널수(변수 개수)
+#nn.RNN(24, 64, 1, batch_first  = True) 
+
+"""
+num_layers 고려한 변수 개수( 기온)
+input_dim: 고려한 시차(23시차 전부터 현재 시차 까지 총 24개) 
+hidden_dim: RNN 레이어에서 출력될 출력값의 개수
+output_dim: 최종적으로 출력할 개수(1시간뒤 2시간뒤 3시간뒤 총 3개)
+"""
+# 아래는 기본틀
+# class RNNModel(pl.LightningModule):
+#     def __init__(
+#             self, lr=0.001, 
+#             input_dim = 24, hidden_dim = 64, 
+#             num_layers = 1, output_dim = 3
+#             ):
+#         super().__init__()
+        
+#     def forward(self, x):
+        
+#     def training_step(self):
+        
+#     def validation_step(self):
+        
+#     def configure_optimizers(self):
+    
+####
+#nn.RNN(24, 64, 1, batch_first = True) 
+class RNNModel(pl.LightningModule):
+    def __init__(
+            self, lr=0.001, 
+            input_dim = 24, hidden_dim = 64, 
+            num_layers = 1, output_dim = 3
+            ):
+        super().__init__()
+        self.lr = lr
+        self.rnn = nn.RNN(
+            input_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)        
+    def forward(self, x):
+        # model.predict 함수 구현 부분에 해당
+        x, _ = self.rnn(x.squeeze())
+        x = self.fc(x)
+        return x
+    def training_step(self, batch, batch_index):
+        x, y = batch 
+        y_hat = self(x.unsqueeze(-1))
+        loss = nn.functional.mse_loss(y_hat , y)
+        self.log("train_loss", loss, prog_bar=True, on_epoch=True)
+        return loss 
+    def validation_step(self, batch, batch_index):
+        x, y = batch 
+        y_hat = self(x.unsqueeze(-1))
+        loss = nn.functional.mse_loss(y_hat , y)
+        self.log("val_loss", loss, prog_bar=True, on_epoch=True)
+        return loss 
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr = self.lr)
+
+x.shape
+model = RNNModel()
+model(x)
+x,y= next(iter(tr_dataloader))
+x.shape
+trainer = pl.Trainer(max_epochs=20)
+trainer.fit(model, tr_dataloader, val_dataloader)
---
+++ python_Spyder_example/250903_순환신경망_tensorflow_최종.py
@@ -0,0 +1,293 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  3 10:00:32 2025
+
+@author: human
+"""
+# 교재 544~630 page 
+import glob
+import pandas as pd
+### 3개 파일 
+files = glob.glob("E:/choinamhoe/csv/**/*csv",recursive=True)
+pd.read_csv(files[0], encoding="cp949")
+
+## 3개 읽어와서 하나로 합치고 df 라는 이름으로 만들기
+## 10분 시작
+
+df1 = pd.read_csv(files[0], encoding="cp949")
+df2 = pd.read_csv(files[1], encoding="cp949")
+df3 = pd.read_csv(files[2], encoding="cp949")
+
+df = pd.concat([df1,df2,df3],ignore_index=True)
+
+dfs = []
+for file in files:
+    df = pd.read_csv(file, encoding="cp949")
+    dfs.append(df)
+df = pd.concat(dfs,ignore_index=True)
+
+df = pd.concat(
+    [pd.read_csv(i, encoding="cp949") for i in files],
+    ignore_index = True)
+
+df = df.iloc[:, 2:]
+df.columns = ["times", "temp"]
+df["times"] = pd.to_datetime(df["times"])
+min_dt = df["times"].min()
+max_dt = df["times"].max()
+# 시작 시간부터 끝 시간까지 특정 간격으로 시간 데이터 모두 만들어달라
+all_dt = pd.date_range(min_dt, max_dt, freq="h")
+time_df = pd.DataFrame({"times":all_dt})
+
+final_df = pd.merge(df, time_df, on="times", how="outer")
+final_df.isna().sum() # 결측치 수 
+na_idx = final_df["temp"].isna()
+final_df[final_df["temp"].isna()] # 결측자료 확인
+
+final_df["temp"] = final_df["temp"].interpolate(method="linear")
+final_df["temp"].interpolate(method="linear")[na_idx]
+final_df["temp"].bfill()[na_idx]
+final_df["temp"].ffill()[na_idx]
+
+from sklearn.preprocessing import MinMaxScaler
+scale = MinMaxScaler(feature_range=(-1, 1))
+train_idx = final_df["times"].dt.year!=2025
+# 2023/2024년도 데이터 기준으로 -1~1 사이로 스케일링 기준 설정
+scale.fit(final_df.loc[train_idx, ["temp"]]) 
+
+# 2023/2024년도 데이터 기준으로 스케일링 수행
+final_df.loc[train_idx, ["temp"]] = scale.transform(
+    final_df.loc[train_idx, ["temp"]])
+# 2025년도 데이터 기준으로 스케일링 수행
+final_df.loc[~train_idx, ["temp"]] = scale.transform(
+    final_df.loc[~train_idx, ["temp"]])
+
+# 32분 시작
+sample = final_df[:3].copy()
+sample.shift(1)
+pd.concat([sample, sample["temp"].shift(1), sample["temp"].shift(2)], axis=1)
+
+concat_df= pd.concat([
+    final_df["temp"].shift(23),
+    final_df["temp"].shift(22),
+    final_df["temp"].shift(21),
+    final_df["temp"].shift(20),
+    final_df["temp"].shift(19),
+    final_df["temp"].shift(18),
+    final_df["temp"].shift(17),
+    final_df["temp"].shift(16),
+    final_df["temp"].shift(15),
+    final_df["temp"].shift(14),
+    final_df["temp"].shift(13),
+    final_df["temp"].shift(12),
+    final_df["temp"].shift(11),
+    final_df["temp"].shift(10),
+    final_df["temp"].shift(9),
+    final_df["temp"].shift(8),
+    final_df["temp"].shift(7),
+    final_df["temp"].shift(6),
+    final_df["temp"].shift(5),
+    final_df["temp"].shift(4),
+    final_df["temp"].shift(3),
+    final_df["temp"].shift(2),
+    final_df["temp"].shift(1),
+    final_df["temp"],
+    final_df["temp"].shift(-1),
+    final_df["temp"].shift(-2),
+    final_df["temp"].shift(-3),
+],axis=1)
+
+concat_df.columns = [f"lag_{i}" for i in list(range(23,-4, -1))]
+
+# 40분 시작
+# 학습데이터 형태로 변환 
+# 23시간 전 22시간전 21시간전 ... 현재시간 1시간후 2시간후 3시간후
+dfs = []
+for i in list(range(23,-4, -1)):
+    _df = final_df[["temp"]].shift(i)
+    _df.columns = [f"lag_{i}"]
+    dfs.append(_df)
+concat_df = pd.concat(dfs,axis=1)
+# 시간 변수 추가
+concat_df["times"] = final_df["times"]
+concat_df = concat_df.dropna()
+concat_df = concat_df.reset_index(drop=True)
+concat_df.iloc[:,:24] # 23시간전 ~ 현재시간
+concat_df.iloc[:,24:-1] # 1시간후 ~ 3시간후
+
+
+x_index = range(24)
+y_index = range(24, 27)
+x = concat_df.iloc[:,x_index] # 23시간전 ~현재시간
+y = concat_df.iloc[:,y_index] # 1시간후 ~ 3시간후
+
+from sklearn.model_selection import train_test_split
+# 2023/2024년도 데이터(Train data/Valid data)
+tr_df = concat_df[concat_df["times"].dt.year!=2025] 
+tr_df, val_df = train_test_split(
+    tr_df , test_size=0.2, random_state=42)
+# 2025년도 데이터(Test data)
+te_df = concat_df[concat_df["times"].dt.year==2025]
+
+tr_df.shape, val_df.shape, te_df.shape
+### 12분 시작
+
+index = 0
+batch_size = 4
+st = index*batch_size
+ed = (index+1)*batch_size
+x = tr_df.iloc[st:ed,x_index].values
+y = tr_df.iloc[st:ed,y_index].values
+
+# 24 분까지 구현
+# 데이터와 index 집어넣으면 batch_size 에 맞게 x,y 추출하는 함수
+def getitem_fun(df, index, batch_size, x_index, y_index):
+    st = index*batch_size
+    ed = (index+1)*batch_size
+    x = df.iloc[st:ed,x_index].values
+    y = df.iloc[st:ed,y_index].values
+    return x, y
+
+x, y = getitem_fun(tr_df, 0, 32, x_index, y_index)
+x.shape, y.shape
+
+df.shape[0]/batch_size
+
+import tensorflow as tf
+import numpy as np 
+class RNNGenerator(tf.keras.utils.Sequence):
+    def __init__(
+            self, df, batch_size, x_index, y_index, getitem_fun):
+        self.df = df 
+        self.batch_size = batch_size
+        self.x_index = x_index
+        self.y_index = y_index
+        self.getitem_function = getitem_fun
+    def __len__(self):
+        # 배치크기를 기준으로 1에포크 도는데 필요한 반복수
+        # 배치크기가 4면 4를 기준으로 전체 데이터수를 나눈 값 +1
+        return int(np.ceil(df.shape[0]/self.batch_size))
+    def __getitem__(self, index):
+        x, y =self.getitem_function(
+            self.df, index,  self.batch_size, 
+            self.x_index, self.y_index)
+        return x, y
+    def on_epoch_end(self):
+        self.df.sample(frac=1)
+
+# 제너레이터 구현
+tr_gen = RNNGenerator(
+    tr_df,  32, x_index, y_index, 
+    getitem_fun=getitem_fun)
+val_gen = RNNGenerator(
+    val_df,  32, x_index, y_index, 
+    getitem_fun=getitem_fun)
+
+x,y = next(iter(tr_gen))
+
+for x,y in tr_gen:
+    break
+
+x.shape,y.shape
+# RNN 모델 
+# 23시간전부터 현재시간 자료를 입력
+inputs = tf.keras.layers.Input(shape = (24, 1))
+x = tf.keras.layers.SimpleRNN(64, activation="tanh")(inputs)
+# 64개에 상태로 반환
+x = tf.keras.layers.Dense(32, activation="relu")(x)
+outputs = tf.keras.layers.Dense(3)(x) # 1시간후~3시간후 예측
+model = tf.keras.Model(inputs, outputs)
+model.summary()
+
+optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
+loss = tf.keras.losses.MeanSquaredError()
+
+model.compile(
+    optimizer  = optimizer,
+    loss = loss,
+    metrics= ["MAE"]
+    )
+
+epochs = 10
+hist = model.fit(
+    tr_gen, 
+    validation_data =val_gen,
+    epochs= epochs,
+    )
+
+pred = model.predict(te_df.iloc[:,:24].values)
+# 스케일링 되었던 값(-1~1)을 원래 스케일(기온범위)로 복원
+y_true = scale.inverse_transform(te_df.iloc[:,24:-1].values)
+y_pred = scale.inverse_transform(pred)
+
+from sklearn.metrics import mean_absolute_error
+mean_absolute_error(y_true, y_pred)
+
+# 25분 진행
+####################################
+# LSTM
+inputs = tf.keras.layers.Input(shape = (24, 1))
+x = tf.keras.layers.LSTM(64, return_sequences = True)(inputs)
+x = tf.keras.layers.LSTM(64, return_sequences = False)(x)
+x = tf.keras.layers.Dense(32, activation="relu")(x)
+outputs = tf.keras.layers.Dense(3)(x)
+model = tf.keras.Model(inputs, outputs)
+model.summary()
+
+optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
+loss = tf.keras.losses.MeanSquaredError()
+
+model.compile(
+    optimizer  = optimizer,
+    loss = loss,
+    metrics= ["MAE"]
+    )
+
+epochs = 10
+hist = model.fit(
+    tr_gen, 
+    validation_data =val_gen,
+    epochs= epochs,
+    )
+
+pred = model.predict(te_df.iloc[:,:24].values)
+y_true = scale.inverse_transform(te_df.iloc[:,24:-1].values)
+y_pred = scale.inverse_transform(pred)
+
+from sklearn.metrics import mean_absolute_error
+mean_absolute_error(y_true, y_pred)
+
+# 35분 시작
+###############################################
+# GRU
+# 교재 544~630 page
+inputs = tf.keras.layers.Input(shape = (24, 1))
+x = tf.keras.layers.GRU(64, return_sequences = True)(inputs)
+x = tf.keras.layers.GRU(64, return_sequences = False)(x)
+x = tf.keras.layers.Dense(32, activation="relu")(x)
+outputs = tf.keras.layers.Dense(3)(x)
+model = tf.keras.Model(inputs, outputs)
+model.summary()
+
+optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
+loss = tf.keras.losses.MeanSquaredError()
+
+model.compile(
+    optimizer  = optimizer,
+    loss = loss,
+    metrics= ["MAE"]
+    )
+
+epochs = 10
+hist = model.fit(
+    tr_gen, 
+    validation_data =val_gen,
+    epochs= epochs,
+    )
+
+pred = model.predict(te_df.iloc[:,:24].values)
+y_true = scale.inverse_transform(te_df.iloc[:,24:-1].values)
+y_pred = scale.inverse_transform(pred)
+
+from sklearn.metrics import mean_absolute_error
+mean_absolute_error(y_true, y_pred)
